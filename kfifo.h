fe80bf057df0656c6647295d5b49ba2000286ba4
Roc <597540267@qq.com>
2020-03-27 18:15:19 +0800

    Create kfifo.h

---
+++ kfifo.h
@@ -0,0 +1,10 @@
+#ifndef _KFIFO_H_
+#define _KFIFO_H_
+
+void kfifo_init(struct kfifo *fifo, unsigned char *buffer, unsigned int size);
+
+unsigned int kfifo_put_byte(struct kfifo *fifo, unsigned char b);
+unsigned int kfifo_put(struct kfifo *fifo, const unsigned char *buffer, unsigned int len);
+unsigned int kfifo_get(struct kfifo *fifo, unsigned char *buffer, unsigned int len);
+
+#endif
