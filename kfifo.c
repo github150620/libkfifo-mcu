09900f242bee2addfa1726a924944da52692653a
Roc <597540267@qq.com>
2020-05-09 17:05:19 +0800

    Update kfifo.c

--- kfifo.c
+++ kfifo.c
@@ -16,24 +16,24 @@ int kfifo_init(struct kfifo *fifo, unsigned char *buffer, unsigned int size) {
 	fifo->size    = size;
 	fifo->in      = 0;
 	fifo->out     = 0;
-	
+
 	return 0;
 }
 
 int kfifo_put(struct kfifo *fifo, char c) {
 	if (fifo->in + 1 == fifo->out) return -1;
-	
+
 	fifo->buffer[fifo->in & (fifo->size - 1)] = c;
 	fifo->in++;
-	
+
 	return 0;
 }
 
-int kfifo_get(struct kfifo *fifo, char *p) {
+int kfifo_get(struct kfifo *fifo, char *c) {
 	if (fifo->in == fifo->out) return -1;
-	
-	*p = fifo->buffer[fifo->out & (fifo->size - 1)];
+
+	*c = fifo->buffer[fifo->out & (fifo->size - 1)];
 	fifo->out++;
-	
+
 	return 0;
 }
